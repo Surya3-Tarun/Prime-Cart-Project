e47a2119b42a634ede8b6e6ffe721ced74751948
Surya <kottapallisuryatarun@gmail.com>
2026-04-25 12:46:37 +0530

    updated settings

--- primecart/settings.py
+++ primecart/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-primecart-secret-key'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
