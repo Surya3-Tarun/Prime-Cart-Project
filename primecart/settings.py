829e863e18ff11ad517dd2eb10128dfacbbdb552
Surya <kottapallisuryatarun@gmail.com>
2026-04-25 13:38:15 +0530

    fixed BASE_DIR error

--- primecart/settings.py
+++ primecart/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-BASE_DIR = Path(file).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-primecart-secret-key'
